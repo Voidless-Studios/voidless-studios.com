2369991005cf745d2934676ca40ec3687077a87e
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-05-11 19:40:02 -0400

    update; add routing

--- voidless/src/app/app-routing.module.ts
+++ voidless/src/app/app-routing.module.ts
@@ -2,8 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Pages
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'Home'
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
