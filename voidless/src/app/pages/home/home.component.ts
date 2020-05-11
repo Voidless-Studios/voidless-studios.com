d0df76d763470f5d25ea1f8519fc87368a56e760
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-05-11 19:40:35 -0400

    update; add jarallax and cover to home page

--- voidless/src/app/pages/home/home.component.ts
+++ voidless/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { jarallax } from 'jarallax';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,10 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.4
+    });
   }
 
 }
