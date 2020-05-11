7ce77043084a4f81239355e916d55385cb3aba5a
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-05-11 19:45:08 -0400

    update; hashtag strategy

--- voidless/src/app/app.module.ts
+++ voidless/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy  } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,12 @@ import { HomeComponent } from './pages/home/home.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide : LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
